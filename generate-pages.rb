d112e4174f0acaa583b42e62c6675d4a496baf1e
vallode <18506096+vallode@users.noreply.github.com>
2022-07-23 13:52:55 +0200

    Restructure slugify

--- generate-pages.rb
+++ generate-pages.rb
@@ -65,8 +65,13 @@ WEAPON_IMAGE = {
 }
 
 def slugify(value)
-  value = value.downcase.strip.gsub(/(?<!\s)(?!\w)'(?=\w)/, "-").gsub(/[\s\,\.]{1}/, '-').gsub(/[\'\(\)]/, "").gsub('+', '-plus')
-  value = value.gsub('ä','a').gsub('ö','o').gsub('ü','u')
+  value = value.downcase.strip
+  value = value.gsub(/(?<!\s)(?!\w)'(?=\w)/, "-")
+  value = value.gsub(/\s\&\s/, " ")
+  value = value.gsub(/[\'\(\)]/, "")
+  value = value.gsub(/[\s\,\.]{1}/, '-')
+  value = value.gsub("ä", "a").gsub("ö", "o").gsub("ü", "u")
+  value = value.gsub("+", "-plus")
 end
 
 def parseElements(elements)
@@ -74,7 +79,7 @@ def parseElements(elements)
 
   if elements != "N/A"
     elements.scan(/([A-Za-z]+)\s([0-9]+)/) do |element, attack|
-      parsedElements.concat("{\"name\" = \"#{element}\", \"attack\" = #{attack}}")
+      parsedElements.concat("{\"name\" = \"#{element}\", \"attack\" = #{attack}},")
     end
   end
 
