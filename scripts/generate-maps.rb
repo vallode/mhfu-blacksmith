6a7eb68e9a27a1c9211944557f5116e72b3f27e1
vallode <18506096+vallode@users.noreply.github.com>
2022-09-02 23:01:03 +0200

    Fix heaven & earth dual blade slug

--- scripts/generate-maps.rb
+++ scripts/generate-maps.rb
@@ -43,7 +43,7 @@ def slugify(value)
   value = value.gsub(/\s\&\s/, " ")
   value = value.gsub(/[\'\"\(\)]/, "")
   value = value.gsub(/\.\s/, "-")
-  value = value.gsub(/[\s\,\.]{1}/, '-')
+  value = value.gsub(/[\s\,\.\&]{1}/, '-')
   value = value.gsub("ä", "a").gsub("ö", "o").gsub("ü", "u").gsub("á", "a")
   value = value.gsub("+", "-plus")
 end
