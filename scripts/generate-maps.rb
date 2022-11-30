783acbc63566d68229ce6f0aed73d2ba89eb987a
vallode <18506096+vallode@users.noreply.github.com>
2022-11-30 18:11:46 +0100

    Fix map generation

--- scripts/generate-maps.rb
+++ scripts/generate-maps.rb
@@ -76,7 +76,7 @@ end
 
 WEAPON_PAIRS.each do |array|
   output = ERB.new(FILE_TEMPLATE, trim_mode: "<>")
-  weapon_files = Dir.glob("content/{blacksmith,armorsmith}/{#{array.join(",")}}/*-crafting.json")
+  weapon_files = Dir.glob("content/{blacksmith,armorsmith}/{#{array.join(",")}}/*-{crafting,source}.json")
   file = File.open(weapon_files[0])
   weapons = JSON.load(file)["weapons"]
 
